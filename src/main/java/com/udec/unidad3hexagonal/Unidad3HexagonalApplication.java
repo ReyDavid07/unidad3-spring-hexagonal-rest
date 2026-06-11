eb1841cc15b09536a65e41efb0f1c9e3cd504c17
Rey David Borrego Perez <rborregop@unicartagena.edu.co>
2026-06-11 17:43:37 -0500

    chore: agregar configuracion inicial de la aplicacion REST

---
+++ src/main/java/com/udec/unidad3hexagonal/Unidad3HexagonalApplication.java
@@ -0,0 +1,11 @@
+package com.udec.unidad3hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unidad3HexagonalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Unidad3HexagonalApplication.class, args);
+    }
+}
